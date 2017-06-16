41cbe6aed3ee05ca38b62af32e13a04acc890e31
hyperflavoured <mahirpanther28@gmail.com>
2017-06-17 00:59:01 +0600

    working skeleton

--- routes/user.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var user = require("../models/user");
-var jwt = require("jwt-simple"); 
-/*===========================================================
-=            Temp            =
-===========================================================*/
